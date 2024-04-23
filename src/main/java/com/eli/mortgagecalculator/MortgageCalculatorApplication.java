1669def071a96497cf1d4e4552e425068928c24d
Eliezer Gyan <eliezer.gyan@amalitech.com>
2024-04-23 15:20:08 +0000

    removed log

--- src/main/java/com/eli/mortgagecalculator/MortgageCalculatorApplication.java
+++ src/main/java/com/eli/mortgagecalculator/MortgageCalculatorApplication.java
@@ -8,7 +8,6 @@ public class MortgageCalculatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MortgageCalculatorApplication.class, args);
-		System.out.println("Hello Mortgage Calculator");
 	}
 
 }
